9f2528ed1ea17896af78551ec9c8cc47d4f6defc
WebMohit <2003mohitkhemani@gmail.com>
2024-12-07 22:00:41 +0530

    loader updated

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React ,{useState} from "react";
 import Lookup from "./components/Lookup";
 import FetchData from "./components/FetchData";
 import HandleDeatils from "./components/HandleDetails";
-
+import Loader from "./components/Loader";
 import "./style/app.css"
 
 const App = ()=>{
     let [postOffice,setPostOffice] = useState(null);
     let [ pincode,setPincode] = useState("");
     let [toggle,setToggle] = useState(true);
+    let [toggleLoader,setToggleLoader] = useState(true);
     return(
         <div className="appContainer" >
            { toggle && <Lookup setPincode={setPincode} setToggle={setToggle} />}
-           {pincode && <FetchData  pincode={pincode} setPostOffice={setPostOffice} /> } 
+           { toggleLoader&&pincode&&!postOffice&&<Loader /> }
+           {pincode && <FetchData  pincode={pincode} setPostOffice={setPostOffice} setToggleLoader={setToggleLoader} /> } 
            {postOffice && <HandleDeatils postOffice={postOffice} pincode={pincode} /> }
         </div>
     )
--- src/components/FetchData.js
+++ src/components/FetchData.js
@@ -1,12 +1,13 @@
 import React,{useEffect, useState} from "react";
 import axios from 'axios';
-const FetchData = ( { pincode,setPostOffice } )=>{
+const FetchData = ( { pincode,setPostOffice,setToggleLoader } )=>{
     let [error,setError] = useState(null);
     function getData(){
         axios.get(`https://api.postalpincode.in/pincode/${pincode}`)
         .then( (response)=> {
             let res = response?.data[0] 
              setPostOffice(res);
+             setToggleLoader((prev)=>!prev);
         })
         .catch( (err)=>{
             return setError(err)
